5dd04d47085579041f73f1cc8970a6513565608d
Shane Yu <shaneyu07@gmail.com>
2021-08-12 09:24:29 +0800

    fix iteration calculator

--- calculate_iterations.py
+++ calculate_iterations.py
@@ -1,5 +1,7 @@
 import sys
 
+from typing import List, Literal
+
 from constants import (
     PARAMS_LOWER_LIMITS,
     PARAMS_UPPER_LIMITS,
@@ -9,33 +11,47 @@ from constants import (
 )
 
 
+REMOVED_PARAMS: List[Literal['period', 'amplification', 'long_take_profit']] = [
+    'period',
+    'amplification',
+    'long_take_profit',
+]
+
+
 iterations = 1.0
 param_iter = {}
 
-for param in PARAMS:
-    iterations = iterations * (
-        (float(PARAMS_UPPER_LIMITS[param]) - PARAMS_LOWER_LIMITS[param])
-        / INCREASE_SIZE[param]
-    )
-    param_iter[param] = (
-        float(PARAMS_UPPER_LIMITS[param]) - PARAMS_LOWER_LIMITS[param]
-    ) / INCREASE_SIZE[param]
+for param in REMOVED_PARAMS:
+    iterations_of_this_param = (
+        float(PARAMS_UPPER_LIMITS[param]) - float(PARAMS_LOWER_LIMITS[param])
+    ) / float(INCREASE_SIZE[param])
 
-total_hours = ((iterations * 2.0) / 60.0) / 60.0
+    iterations = iterations * iterations_of_this_param
+    param_iter[param] = iterations_of_this_param
+
+total_hours = ((iterations * SEC_TO_SLEEP_PER_ITERATION) / 60.0) / 60.0
 total_days = total_hours / 24.0
 
-for param in PARAMS:
+for param in REMOVED_PARAMS:
     print(
         '{} ({} -> {} | move {} per iter): {} iterations'.format(
             param,
-            int(PARAMS_LOWER_LIMITS[param]),
-            int(PARAMS_UPPER_LIMITS[param]),
-            int(INCREASE_SIZE[param]),
-            int(param_iter[param]),
+            float(PARAMS_LOWER_LIMITS[param])
+            if param == 'amplification'
+            else int(PARAMS_LOWER_LIMITS[param]),
+            float(PARAMS_UPPER_LIMITS[param])
+            if param == 'amplification'
+            else int(PARAMS_UPPER_LIMITS[param]),
+            float(INCREASE_SIZE[param])
+            if param == 'amplification'
+            else int(INCREASE_SIZE[param]),
+            float(param_iter[param])
+            if param == 'amplification'
+            else int(param_iter[param]),
         )
     )
 
 print('total iterations:', int(iterations))
-print('{} sec / per iteration'.format(int(SEC_TO_SLEEP_PER_ITERATION)))
+print('{} sec / per iteration'.format(float(SEC_TO_SLEEP_PER_ITERATION)))
 print('total time (hours):', total_hours)
 print('total time (days):', total_days)
